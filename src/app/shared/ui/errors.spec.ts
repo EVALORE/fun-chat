d080797f64faf37ae11c74b83ca11460de8fc0f1
Vladimir Eremia <vladimireremia377@gmail.com>
2025-06-05 16:20:12 +0300

    chore(tests): remove unit tests for  components and services
    - Deleted test files for `App`, `Auth`, `AuthValidation`, `Errors`, and `WebSocketClient`

--- src/app/shared/ui/errors.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Errors } from './errors';
-
-describe('Errors', () => {
-  it('should create an instance', () => {
-    const directive = new Errors();
-    expect(directive).toBeTruthy();
-  });
-});
