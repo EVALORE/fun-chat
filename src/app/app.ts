288de73462b130248cf09a9a7d52ea7b50f5dd84
Vladimir Eremia <vladimireremia377@gmail.com>
2025-06-14 04:19:04 +0300

    feat(app): add header and footer with improved layout styling
    - Introduced a new `header` component with title, user avatar, and logout button using Taiga UI.
    - Added a `footer` with a GitHub link and static year display.
    - Updated `AppComponent` template and styles for enhanced layout and responsive design.
    - Modified `Login` styling for better vertical alignment and scroll behavior.
    - Enhanced `AppComponent` imports to include additional Taiga UI modules for headers, buttons, icons, and avatars.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
-import { TuiRoot } from '@taiga-ui/core';
+import { TuiButton, TuiIcon, TuiRoot, TuiTitle } from '@taiga-ui/core';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Login } from './features/auth/login';
+import { TuiAvatar } from '@taiga-ui/kit';
+import { TuiHeader } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [TuiRoot, Login],
+  imports: [TuiRoot, Login, TuiTitle, TuiButton, TuiAvatar, TuiHeader, TuiIcon],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
