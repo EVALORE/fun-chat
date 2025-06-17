6a87adf4634281d051ed852f877b571b97ab8cdb
Vladimir Eremia <vladimireremia377@gmail.com>
2025-06-17 22:17:19 +0300

    feat(app): add Main component and update routes
    - Created `Main` component with basic template and structure.
    - Registered `Main` component in `app.routes.ts` with lazy loading.
    - Added default and wildcard route redirections to `login`.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login').then((m) => m.Login),
+  },
+  {
+    path: 'main',
+    loadComponent: () => import('./features/main/main').then((m) => m.Main),
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
