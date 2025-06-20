0087e256b6ad1b6a279ba18d390f873c5ef7c19c
Vladimir Eremia <vladimireremia377@gmail.com>
2025-06-20 18:48:53 +0300

    feat(user-list): implement user search functionality

--- src/app/features/main/user-list/user-list.ts
+++ src/app/features/main/user-list/user-list.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { WebSocketClient } from '../../../core/api/web-socket-client';
 import { combineLatest, map } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
@@ -6,16 +6,26 @@ import { TuiButton, TuiTextfield } from '@taiga-ui/core';
 import { TuiCell, TuiSearch } from '@taiga-ui/layout';
 import { UserStore } from '../../../core/user-store';
 import { TuiStatus } from '@taiga-ui/kit';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-user-list',
-  imports: [AsyncPipe, TuiCell, TuiStatus, TuiSearch, TuiTextfield, ReactiveFormsModule, TuiButton],
+  imports: [
+    AsyncPipe,
+    TuiCell,
+    TuiStatus,
+    TuiSearch,
+    TuiTextfield,
+    ReactiveFormsModule,
+    TuiButton,
+    FormsModule,
+  ],
   templateUrl: './user-list.html',
   styleUrl: './user-list.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserList {
+export class UserList implements OnInit {
   private readonly ws = inject(WebSocketClient);
   private readonly user = inject(UserStore);
 
@@ -24,27 +34,30 @@ export class UserList {
   public readonly users = combineLatest([
     this.ws.onType('USER_ACTIVE'),
     this.ws.onType('USER_INACTIVE'),
+    toObservable(this.search),
   ]).pipe(
-    map(([active, inactive]) =>
+    map(([active, inactive, search]) =>
       [...active.payload.users, ...inactive.payload.users].filter(
-        (user) => user.login !== this.user.login(),
+        (user) => user.login !== this.user.login() && user.login.includes(search),
       ),
     ),
   );
 
-  constructor() {
-    setTimeout(() => {
-      this.ws.send({
-        id: String(Date.now()),
-        type: 'USER_ACTIVE',
-        payload: null,
-      });
+  public searchChange(value: string): void {
+    this.search.set(value);
+  }
 
-      this.ws.send({
-        id: String(Date.now()),
-        type: 'USER_INACTIVE',
-        payload: null,
-      });
-    }, 1000);
+  public ngOnInit(): void {
+    this.ws.send({
+      id: String(Date.now()),
+      type: 'USER_ACTIVE',
+      payload: null,
+    });
+
+    this.ws.send({
+      id: String(Date.now()),
+      type: 'USER_INACTIVE',
+      payload: null,
+    });
   }
 }
